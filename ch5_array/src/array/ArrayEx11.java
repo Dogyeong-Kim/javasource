b98553ad2b207b09c7bbce30bdd31e42ec6fc17d
Kim Dogyeong <pcs6389@gmail.com>
2025-03-04 15:02:24 +0900

    배열 예제 완성

---
+++ ch5_array/src/array/ArrayEx11.java
@@ -0,0 +1,9 @@
+package array;
+
+public class ArrayEx11 {
+    public static void main(String[] args) {
+        int[] score = { 75, 88, 91, 33, 100, 55, 95 };
+
+        // 정렬(오름, 내림) : 메소드 이용
+    }
+}
