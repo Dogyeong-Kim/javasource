3741c5ae4188deead83cd7fb5af23b3b01e91f86
Kim Dogyeong <pcs6389@gmail.com>
2025-03-07 17:21:04 +0900

    클래스 예제

---
+++ ch6_oop/src/oop/FinalTest.java
@@ -0,0 +1,18 @@
+package oop;
+
+// class Test extends FinalTest {
+// }
+
+public class FinalTest {
+    final int MAX_SIZE = 10;
+
+    final void getMaxSize() {
+        // MAX_SIZE = 20;
+        final int LV = MAX_SIZE;
+        // LV = 20
+    }
+
+    class Test extends FinalTest {
+
+    }
+}
---
+++ ch6_oop/src/oop/ImportEx.java
@@ -0,0 +1,35 @@
+package oop;
+// import 문
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+import static java.lang.Math.*;
+
+// import 사용하지 않는 패키지(자동으로 import 처리 해줌)
+// java.lang.* (lang 패키지에 속한 모든 클래스)
+
+// 다른 패키지의 클래스 사용할 때
+
+import extend.Parent;
+
+public class ImportEx {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Parent parent = new Parent();
+
+        Date date = new Date();
+        System.out.println(date); // Fri Mar 07 16:40:50 KST 2025
+
+        SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-dd");
+        System.out.println("오늘 날짜는 : " + sdf.format(date));
+
+        // Math : static 메소드로 선언됨
+        // System.out.println(Math.min(15, 25));
+        System.out.println(min(15, 25));
+        // System.out.println(Math.random());
+        System.out.println(random());
+
+        String str;
+    }
+}
